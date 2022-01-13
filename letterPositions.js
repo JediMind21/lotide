3988fd9e0c59d42ee761f4575006e69e73d8bf95
Brandon <brandon.dahlberg21@gmail.com>
2022-01-13 21:48:40 +0000

    adding letterPositions method to lotide

---
+++ letterPositions.js
@@ -0,0 +1,20 @@
+let str = "I like to listen to my music really loud";
+
+const letterPositions = function (input) {
+  const results = {};
+  for (let x = 0; x < input.length; x++) {
+    const char = input[x];
+    if (char !== " ") {
+      //check out MDN
+      if (!Array.isArray(results[char])) {
+        results[char] = [x];
+      } else {
+        results[char].push(x);
+      }
+    }
+  }
+
+  return results;
+};
+
+console.log(letterPositions(str));
