8bfdd066a8f518b798e200d02ae78ad3fd195b0d
Brandon <brandon.dahlberg21@gmail.com>
2022-01-13 22:14:26 +0000

    adding eqObjects method to lotide library

--- eqObjects.js
+++ eqObjects.js
@@ -1,34 +1,34 @@
-const assertArraysEqual = function (arr1, arr2) {
-  arr1Str = "";
-  arr2Str = "";
-  for (num of arr1) {
-    num.toString();
-    arr1Str += num;
-  }
-  for (num of arr2) {
-    num.toString();
-    arr2Str += num;
-  }
-  if (arr1Str !== arr2Str) {
+const eqArrays = function (arr1, arr2) {
+  let str1 = "";
+  let str2 = "";
+  if (arr1.length !== arr2.length) {
     return false;
-  } else if (arr1Str === arr2Str) {
-    return true;
+  } else if (arr1.length === arr2.length) {
+    for (let x = 0; x < arr1.length; x++) {
+      if (typeof arr1[x] === typeof arr2[x]) {
+        str1 += arr1[x];
+        str2 += arr2[x];
+      } else {
+        return false;
+      }
+    }
+    if (str1 === str2) {
+      return true;
+    } else return false;
   }
 };
 
 const eqObjects = function (obj1, obj2) {
-  let arrKeys1 = Object.keys(obj1);
-  let arrKeys2 = Object.keys(obj2);
-  let arrVals1 = Object.values(obj1);
-  let arrVals2 = Object.values(obj2);
+  let keys1 = Object.keys(obj1);
+  let keys2 = Object.keys(obj2);
+  let vals1 = Object.values(obj1);
+  let vals2 = Object.values(obj2);
   if (Object.keys(obj1).length !== Object.keys(obj2).length) {
     return false;
-  } else if (
-    assertArraysEqual(arrKeys1, arrKeys2) &&
-    assertArraysEqual(arrKeys1, arrKeys2) &&
-    assertArraysEqual(arrKeys1, arrKeys2)
-  ) {
-    return true;
+  } else {
+    if (eqArrays(keys1, keys2) && eqArrays(vals1, vals2)) {
+      return true;
+    } else return false;
   }
 };
 
@@ -37,4 +37,7 @@ const dc = { d: ["2", 3], c: "1" };
 console.log(eqObjects(cd, dc)); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2));
+const cd3 = { c: "1", d: ["2", 3, 4] };
+const cd4 = { c: "1", d: [3, 4] };
+console.log(eqObjects(cd3, cd2));
+console.log(eqObjects(cd3, cd4));
