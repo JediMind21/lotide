de229ebad10bc5cd5842ced819462ccaac6eff2f
Brandon <brandon.dahlberg21@gmail.com>
2022-01-13 16:55:08 +0000

    fixed a bug. It actually works as intended now!

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,26 +2,30 @@ const assertArraysEqual = function (arr1, arr2) {
   let str1 = "";
   let str2 = "";
   if (arr1.length !== arr2.length) {
-    srt1 = false;
+    return "🛑🛑🛑 Assertion Failed: The arrays are NøT equal D=";
   } else if (arr1.length === arr2.length) {
     for (let x = 0; x < arr1.length; x++) {
       if (typeof arr1[x] === typeof arr2[x]) {
         str1 += arr1[x];
         str2 += arr2[x];
       } else {
-        str1 = false;
+        return "🛑🛑🛑 Assertion Failed: The arrays are NøT equal D=";
       }
     }
   }
   if (str1 === str2) {
-    console.log("✅✅✅ Assertion Passed: The arrays are equal!");
-  } else
-    console.log(
-      "🛑🛑🛑 Assertion Failed: The arrays are NøT equal D="
-    );
+    return "✅✅✅ Assertion Passed: The arrays are equal!";
+  } else {
+    return "🛑🛑🛑 Assertion Failed: The arrays are NøT equal D=";
+  }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, "2", 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 5, 3], [1, 2, 3]);
+let words = ["pig", "toilet", "party", "shelter"];
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+console.log(assertArraysEqual([1, "2", 3], [1, 2, 3]));
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+console.log(assertArraysEqual(words, ["pig"]));
+console.log(
+  assertArraysEqual(words, ["pig", "toilet", "party", "shelter"])
+);
