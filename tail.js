f2a76c0c0ba0661d503334404669774dfbac9ad2
Brandon Dahlberg <brandon.dahlberg21@gmail.com>
2022-01-17 16:58:36 -0700

    refactor of tail method

--- tail.js
+++ tail.js
@@ -1,20 +1,8 @@
-const assertEqual = function (actual, expected) {
-  if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  } else if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
-const words = ["Donkey", "Rhino", "Giraffe"];
+const assertArraysEqual = require('./assertEquals');
 
 const tail = function (input) {
-  input.shift();
-  return input;
+	input.shift();
+	return input;
 };
 
-const result = tail(words);
-
-assertEqual(result.length, 2);
-assertEqual(result[0], "Donkey");
-assertEqual(result[0], "Rhino");
+module.exports = tail;
