6fc0ade5abb908c809277a51d81122c263807e4a
Brandon Dahlberg <brandon.dahlberg21@gmail.com>
2022-01-17 18:21:37 -0700

    wrote tests using mocha and chai for these methods

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,7 +1,13 @@
 const middle = require('../middle');
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-let arr = [2, 3, 6, 8, 4, 1];
-
-assertArraysEqual(eqArrays(middle(arr), [6, 8]), true);
+describe(`#Tests for middle method in lotide`, () => {
+	it(`should return the middle 2 values in array if even number of items in array`, () => {
+		let arr = [2, 3, 6, 8, 4, 1];
+		assert.deepEqual(middle(arr), [6, 8]);
+	});
+	it(`should return the middle value in array if odd number of items in array`, () => {
+		let arr = [2, 3, 6, 8, 4];
+		assert.deepEqual(middle(arr), [6]);
+	});
+});
--- test/tail.test.js
+++ test/tail.test.js
@@ -1,4 +1,12 @@
 const tail = require('../tail');
-
-const words = ['Donkey', 'Rhino', 'Giraffe'];
-const result = tail(words);
+const assert = require('chai').assert;
+describe(`#Tests for tail method`, () => {
+	it(`should return all but the first element in an array as a new array`, () => {
+		const arr1 = ['lion', 'monkey', 'dolphin'];
+		assert.deepEqual(tail(arr1), ['monkey', 'dolphin']);
+	});
+	it(`should not return the first element in the original array in the new array`, () => {
+		const arr1 = ['lion', 'monkey', 'dolphin'];
+		assert.notDeepEqual(tail(arr1), ['lion', 'monkey']);
+	});
+});
