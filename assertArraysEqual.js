82002fb9d84a38dbab3a82792524e89083d8b6fc
Brandon Dahlberg <brandon.dahlberg21@gmail.com>
2022-01-17 17:34:19 -0700

    refactor of assertArraysEqual method for lotide library

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,31 +1,9 @@
-const assertArraysEqual = function (arr1, arr2) {
-  let str1 = "";
-  let str2 = "";
-  if (arr1.length !== arr2.length) {
-    return "🛑🛑🛑 Assertion Failed: The arrays are NøT equal D=";
-  } else if (arr1.length === arr2.length) {
-    for (let x = 0; x < arr1.length; x++) {
-      if (typeof arr1[x] === typeof arr2[x]) {
-        str1 += arr1[x];
-        str2 += arr2[x];
-      } else {
-        return "🛑🛑🛑 Assertion Failed: The arrays are NøT equal D=";
-      }
-    }
-  }
-  if (str1 === str2) {
-    return "✅✅✅ Assertion Passed: The arrays are equal!";
-  } else {
-    return "🛑🛑🛑 Assertion Failed: The arrays are NøT equal D=";
-  }
+const assertArraysEqual = function (actual, expected) {
+	if (actual === expected) {
+		return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+	} else {
+		return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+	}
 };
 
-let words = ["pig", "toilet", "party", "shelter"];
-
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
-console.log(assertArraysEqual([1, "2", 3], [1, 2, 3]));
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
-console.log(assertArraysEqual(words, ["pig"]));
-console.log(
-  assertArraysEqual(words, ["pig", "toilet", "party", "shelter"])
-);
+module.exports = assertArraysEqual;
